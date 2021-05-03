b02f4d068783b1a577263357f5d4450aa6968c29
Malcolm Navarro <mmnavarr@live.com>
2021-05-02 22:02:25 -0500

    v0.0.2

--- src/main.ts
+++ src/main.ts
@@ -9,16 +9,20 @@ const WALLET_PRIVATE_KEY = process.env.WALLET_PRIVATE_KEY ?? "ENV_ERROR";
 const LIQUIDITY_POOL_ADDRESS = process.env.LIQUIDITY_POOL_ADDRESS ?? "ENV_ERROR";
 const LIQUIDITY_POOL_ABI = process.env.LIQUIDITY_POOL_ABI ?? "ENV_ERROR";
 
+let provider;
+let wallet;
+
 const main = async () => {
   try {
     console.log("Starting bot")
 
     // Connect to mainnet (homestead) with infura URL
-    const provider = new ethers.providers.JsonRpcProvider(INFURA_PROVIDER_URL);
+    provider = new ethers.providers.JsonRpcProvider(INFURA_PROVIDER_URL);
 
     // Connect to wallet
-    const wallet = new Wallet(WALLET_PRIVATE_KEY).connect(provider);
+    wallet = new Wallet(WALLET_PRIVATE_KEY).connect(provider);
     // const wallet = Wallet.fromMnemonic(WALLET_SEED_PHRASE).connect(provider);
+
     const walletBalance = await wallet.getBalance()
     console.log(`Connected to wallet (${wallet.address}) - ETH balance: ${ethers.utils.formatEther(walletBalance)}`);
 
@@ -31,30 +35,21 @@ const main = async () => {
 }
 
 const detectLiquidityPoolDrip = async () => {
+  // Infinitely loop until balancer LDB is ready for purchase
+  const forever = true;
+  while (forever) {
+    const lpContract = new ethers.Contract(LIQUIDITY_POOL_ADDRESS, LIQUIDITY_POOL_ABI, provider)
+    const isDeployed = await lpContract.deployed();
+    // console.log("🚀 ~ file: main.ts ~ line 39 ~ detectLiquidityPoolDrip ~ isDeployed", isDeployed)
 
-  // Connect to mainnet (homestead) with infura URL
-  const provider = new ethers.providers.JsonRpcProvider(INFURA_PROVIDER_URL);
+    const isFinalized: boolean = (await lpContract.functions.isFinalized())[0];
+    const isPublicSwap: boolean = (await lpContract.functions.isPublicSwap())[0];
 
-  const lpContract = new ethers.Contract(LIQUIDITY_POOL_ADDRESS, LIQUIDITY_POOL_ABI, provider)
-  const isDeployed = await lpContract.deployed();
-  // console.log("🚀 ~ file: main.ts ~ line 39 ~ detectLiquidityPoolDrip ~ isDeployed", isDeployed)
-
-  // const swapPrice = await lpContract.getSwapPrice();
-  // console.log("🚀 ~ file: main.ts ~ line 41 ~ detectLiquidityPoolDrip ~ swapPrice", swapPrice)
-
-  const isFinalized: boolean[] = await lpContract.functions.isFinalized();
-  console.log("🚀 ~ file: main.ts ~ line 47 ~ detectLiquidityPoolDrip ~ isFinalized", isFinalized[0])
-  const isPublicSwap: boolean[] = await lpContract.functions.isPublicSwap();
-  console.log("🚀 ~ file: main.ts ~ line 52 ~ detectLiquidityPoolDrip ~ isPublicSwap", isPublicSwap[0])
-
-  // Compare against existing pre balance to detect for drip then buy otherwise update prev
-  if (!!isDeployed && isFinalized && isPublicSwap) {
-    buy();
-    return;
+    if (!!isDeployed && isFinalized && isPublicSwap && !forever) {
+      buy();
+      return;
+    }
   }
-
-  // Sleep 2 seconds then re-detect
-  setTimeout(detectLiquidityPoolDrip, 2000);
 }
 
 const buy = () => {
